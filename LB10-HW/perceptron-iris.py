1b252f9cd027d332577c3db0edf3d86858942c81
oskarziembrowicz <rakson2002@gmail.com>
2023-05-17 13:33:17 +0200

    Creating 3 perceptrons and plots, one for every flower.

--- LB10-HW/perceptron-iris.py
+++ LB10-HW/perceptron-iris.py
@@ -48,35 +48,66 @@ class Perceptron(object):
         # print(np.where(self.net_input(X) >= 0.0, 1, -1))
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-# == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+# == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+#end of perceptron class
+# == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
 #%matplotlib inline
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-import numpy as np
+# import numpy as np
 
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 print('URL:', s)
 
 df = pd.read_csv(s, header=None, encoding='utf-8')
-df.tail(5)
-y = df.iloc[0:100, 4].values
-y = np.where(y == 'Iris-setosa', -1, 1)
-X = df.iloc[0:100, [0, 2]].values
+#df.tail(5)
+
+# y = df.iloc[0:100, 4].values
+# y = np.where(y == 'Iris-setosa', -1, 1)
+# X = df.iloc[0:100, [0, 2]].values
+
+y = df.iloc[0:, 4].values
+y1 = np.where(y == 'Iris-setosa', -1, 1)
+y2 = np.where(y == 'Iris-versicolor', -1, 1)
+y3 = np.where(y == 'Iris-virginica', -1, 1)
+X = df.iloc[0:, [0,2]].values
+
+#=================================================================================
+#plot points
+#=================================================================================
 
 plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
 plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+
+plt.scatter(X[100:, 0], X[100:, 1], color='green', marker='*', label='virginica')
+
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
 plt.show()
 
 # == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+#plot lines based on perceptrons
+# == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
 
-ppn = Perceptron(learning_parameter=0.1, number_of_iterations=10)
-ppn.fit(X, y)
-plt.plot(range(0, len(ppn.number_of_errors)), ppn.number_of_errors, marker='o')
+# ppn = Perceptron(learning_parameter=0.1, number_of_iterations=10)
+# ppn.fit(X, y)
+# plt.plot(range(0, len(ppn.number_of_errors)), ppn.number_of_errors, marker='o')
+
+ppn1 = Perceptron(learning_parameter=0.1, number_of_iterations=10)
+ppn2 = Perceptron(learning_parameter=0.1, number_of_iterations=10)
+ppn3 = Perceptron(learning_parameter=0.1, number_of_iterations=10)
+
+ppn1.fit(X, y1)
+ppn2.fit(X, y2)
+ppn3.fit(X, y3)
+
+plt.plot(range(0, len(ppn1.number_of_errors)), ppn1.number_of_errors, marker='o', label='setosa', color='red')
+plt.plot(range(0, len(ppn2.number_of_errors)), ppn2.number_of_errors, marker='o', label='versicolor', color='blue')
+plt.plot(range(0, len(ppn3.number_of_errors)), ppn3.number_of_errors, marker='o', label='virginica', color='green')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
+plt.legend(loc='lower left')
 plt.show()
